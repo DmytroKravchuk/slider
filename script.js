49de45edba0ca2a3a6ea2af10ad0698d340c405a
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2020-02-24 00:03:13 +0200

    add main logic

---
+++ script.js
@@ -0,0 +1,82 @@
+"use strict";
+window.onload = function() {
+    
+    class SLider {
+        constructor(slider, thumb) {
+            this.slider = document.querySelector(slider);
+            this.thumb = document.querySelector(thumb);
+
+        }
+
+        events = {
+            "thumbCLick": [
+                {
+                    'target': () => this.slider,
+                    'event': 'click',
+                    'handler': (e) => this.setClickPosition(e)
+                }
+            ],
+            "thumbDrag": [
+                {
+                    'target': () => this.thumb,
+                    'event': 'mousedown',
+                    'handler': (e) => this.setDragPosition(e)
+                },
+                {
+                    'target': () => this.thumb,
+                    'event': 'dragstart',
+                    'handler': () => false
+                }
+            ]
+        }
+
+        init() {
+            this.initEvents(this.events.thumbCLick);
+            this.initEvents(this.events.thumbDrag);
+        }
+
+        initEvents(eventType) {
+            eventType.forEach(el => {
+                el.target().addEventListener(el.event, el.handler)
+            });
+        }
+
+        setClickPosition(e) {
+            if(e.target === this.thumb) return;
+            let cordXSlider = this.slider.getBoundingClientRect().x;
+            let cordXthumb = e.pageX - cordXSlider - this.thumb.offsetWidth / 2;
+            this.thumb.style.left = cordXthumb + 'px';
+        }
+
+        setDragPosition(e) {
+            let shiftX = e.clientX - e.target.getBoundingClientRect().left;
+            
+            document.addEventListener('mousemove', onMouseMove.bind(this));
+            document.addEventListener('mouseup', onMouseUp);
+
+            function onMouseMove(e) {
+                let newLeft = e.clientX - shiftX - this.slider.getBoundingClientRect().x;
+
+                if(newLeft < 0) {
+                    newLeft = 0;
+                }
+
+                let rightEdge = this.slider.offsetWidth - this.thumb.offsetWidth;
+                if(newLeft > rightEdge) {
+                    newLeft = rightEdge;
+                }
+
+                this.thumb.style.left = newLeft + 'px';
+            }
+
+            function onMouseUp() {
+                document.removeEventListener('mousemove', onMouseMove.bind(this));
+                document.removeEventListener('mousemove', onMouseUp);
+            }
+
+        }
+    }
+
+    const slider = new SLider('#slider', '.thumb');
+    slider.init();
+}
