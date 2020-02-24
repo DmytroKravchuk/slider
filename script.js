f05a2eb0dd9aa817e14e5b0524c3512fd4a98062
Dmytro Kravchuk <Dmytro.Kravchuk@infopulse.com>
2020-02-24 17:11:03 +0200

    add(*): added logic for value

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
 "use strict";
 window.onload = function() {
 
-    class SLider {
-        constructor(slider, thumb) {
+    class Slider {
+        constructor(slider, thumb, props) {
             this.slider = document.querySelector(slider);
+            this.sliderWidth = this.slider.offsetWidth;
             this.thumb = document.querySelector(thumb);
-
+            this.config = {
+                min: props.min || 0,
+                max: props.max || 100,
+                step: props.step || 1,
+                start: props.start || [0, 100],
+                isMultiple: props.isMultiple || false
+            }
         }
 
-        events = {
-            "thumbCLick": [
-                {
-                    'target': () => this.slider,
-                    'event': 'click',
-                    'handler': (e) => this.setClickPosition(e)
-                }
-            ],
-            "thumbDrag": [
-                {
-                    'target': () => this.thumb,
-                    'event': 'mousedown',
-                    'handler': (e) => this.setDragPosition(e)
-                },
-                {
-                    'target': () => this.thumb,
-                    'event': 'dragstart',
-                    'handler': () => false
-                }
-            ]
-        };
+        events = [
+            {
+                'target': () => this.slider,
+                'event': 'click',
+                'handler': (e) => this.setClickPosition(e)
+            },
+            {
+                'target': () => this.thumb,
+                'event': 'mousedown',
+                'handler': (e) => this.setDragPosition(e)
+            },
+            {
+                'target': () => this.thumb,
+                'event': 'dragstart',
+                'handler': () => false
+            }
+        ];
 
         init() {
-            this.initEvents(this.events.thumbCLick);
-            this.initEvents(this.events.thumbDrag);
+            this.slider.append(this._createBarValue());
+            this.initEvents();
+            this.setValue(this.config.start);
         }
 
-        initEvents(eventType) {
-            eventType.forEach(el => {
+        initEvents() {
+            this.events.forEach(el => {
                 el.target().addEventListener(el.event, el.handler)
             });
         }
 
         destroy(eventType) {
-            eventType.forEach(el => {
+            this.events.forEach(el => {
                 el.target().removeEventListener(el.event, el.handler)
             });
         }
@@ -51,13 +55,16 @@ window.onload = function() {
             if(e.target === this.thumb) return;
             let cordXSlider = this.slider.getBoundingClientRect().x;
             let cordXthumb = e.pageX - cordXSlider - this.thumb.offsetWidth / 2;
-            this.thumb.style.left = cordXthumb + 'px';
+            if(cordXthumb >= 0) {
+                let value = Math.round(parseFloat(cordXthumb *  this.config.max / this.sliderWidth));
+                this.setValue(value);
+            }
         }
 
         setDragPosition(e) {
             let shiftX = e.clientX - e.target.getBoundingClientRect().left;
             let mouseMove = onMouseMove.bind(this);
-
+            let self = this;
             document.addEventListener('mousemove', mouseMove);
             document.addEventListener('mouseup', onMouseUp);
 
@@ -72,18 +79,53 @@ window.onload = function() {
                 if(newLeft > rightEdge) {
                     newLeft = rightEdge;
                 }
-
-                this.thumb.style.left = newLeft + 'px';
+                let value = Math.round(parseFloat(newLeft *  this.config.max / this.sliderWidth));
+                self.setValue(value);
             }
 
             function onMouseUp() {
                 document.removeEventListener('mousemove', mouseMove);
                 document.removeEventListener('mousemove', onMouseUp);
             }
+        }
 
+        setValue(value) {
+            if(!this.config.isMultiple) {
+                this.bar.style.width = this._calcCorrectValue(value) + 'px';
+                this.barValue.style.left = this._calcCorrectValue(value) + 'px';
+                this.barValue.innerHTML = value;
+                this.thumb.style.left = this._calcCorrectValue(value) + 'px';
+            }
+        }
+
+        _createBarValue() {
+            let wrapValue = document.createElement('div');
+            this.bar = document.createElement('div');
+            this.bar.classList.add('bar-value');
+
+            if(!this.config.isMultiple) {
+                this.barValue = document.createElement('span');
+                this.barValue.classList.add('value');
+                this.barValue.innerText = this.config.start[0];
+                wrapValue.append(this.bar);
+                wrapValue.append(this.barValue);
+            }
+
+            return wrapValue;
+        }
+
+        _calcCorrectValue(value) {
+            return Math.round(parseFloat(value * this.sliderWidth / this.config.max));
         }
     }
 
-    const slider = new SLider('#slider', '.thumb');
+    const slider = new Slider('#slider', '.thumb', {
+        min: 0,
+        max: 150,
+        step: 1,
+        start: [10],
+    });
+
     slider.init();
+
 };
